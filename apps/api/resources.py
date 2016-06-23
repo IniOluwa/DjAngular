0f1646bc5e6c859b9bca524543f726ea80b6a93b
IniOluwa <inioluwafageyinbo@gmail.com>
2016-06-23 13:06:13 +0100

    Resources updated.

--- apps/api/resources.py
+++ apps/api/resources.py
@@ -3,6 +3,11 @@ from ..resources.models import Resources
 
 
 class ResourcesModelResource(ModelResource):
+    """ Api facet """
     class Meta:
         queryset = Resources.objects.all()
-        allowed_methods = ['get']
+        resource_name = 'resource'
+        allowed_methods = ['get', 'post', 'patch', 'delete']
+        # authorization = Authorization()
+        # authentication = Authentication()
+        always_return_data = True
