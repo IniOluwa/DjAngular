eb8c8827c1f1483ca8e4f05d168889c073a5eb49
IniOluwa <inioluwafageyinbo@gmail.com>
2016-06-23 13:05:33 +0100

    Settings updated with static setup and 'djng' added to INSTALLED_APPS.

--- djangular/settings.py
+++ djangular/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
-    'tastypie'
+    # Tastypie
+    'tastypie',
+
+    # Django-Angular
+    'djng',
 ]
 
 LOCAL_APPS = [
@@ -130,3 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'appstatic/app'),
+    os.path.join(BASE_DIR, 'appstatic/libs'),
+)
